3e41060be4a4a4416684b231039b8686c275af5b
Dakshal P <dakshal29@gmail.com>
2023-05-26 18:23:02 -0400

    add components for dark mode toggle

---
+++ components/dashboard/DarkModeToggle.tsx
@@ -0,0 +1,40 @@
+"use client"
+import { BsMoon, BsSun } from "react-icons/bs";
+import Dropdown from "../Dropdown";
+import { Menu } from "@headlessui/react"
+import Button from "../Button";
+
+const MenuItems = (
+    <div className="p-3 flex flex-col gap-2">
+        <Menu.Item>
+                    {({ active }) => (
+                        <Button onClick={() => null} className='w-full flex flex-row gap-2 items-center justify-center' variant={active ? "cgreen" : "cgray"}><BsMoon /> Dark Mode</Button>
+                    )}
+        </Menu.Item>
+        <Menu.Item>
+                    {({ active }) => (
+                        <Button onClick={() => null} className='w-full flex flex-row gap-2 items-center justify-center' variant={active ? "cgreen" : "cgray"}><BsSun /> Light Mode</Button>
+                    )}
+        </Menu.Item>
+        <Menu.Item>
+                    {({ active }) => (
+                        <Button onClick={() => null} className='w-full' variant={active ? "cgreen" : "cgray"}>System Preference</Button>
+                    )}
+        </Menu.Item>
+    </div>
+)
+export default function DarkModeToggle() {
+
+    return (
+        <Dropdown menu={MenuItems}>
+            <div className="cursor-pointer">
+                <BsSun
+                    className="w-6 h-6 dark:hidden"
+                />
+                <BsMoon
+                    className="w-6 h-6 hidden dark:block"
+                />
+            </div>
+        </Dropdown>
+    );
+}
--- components/dashboard/YearTotal.tsx
+++ components/dashboard/YearTotal.tsx
@@ -1,22 +1,13 @@
+import DarkModeToggle from "./DarkModeToggle";
 
-import { BsMoon, BsSun } from "react-icons/bs";
 
 const YearTotal = () => (
-    <div className="mr-auto flex flex-row items-center justify-between w-full">
+    <div className="p-3 mr-auto flex flex-row items-center justify-between w-full">
         <div>
             <p className="text-4xl font-extrabold font-display">75 Hours</p>
             <p>total in 2023</p>
         </div>
-        <div className="p-4 cursor-pointer">
-            <BsSun
-                className="w-6 h-6 dark:hidden"
-                title="You are in light mode"
-            />
-            <BsMoon
-                className="w-6 h-6 hidden dark:block"
-                title="You are in dark mode"
-            />
-        </div>
+        <DarkModeToggle />
     </div>
 );
 
