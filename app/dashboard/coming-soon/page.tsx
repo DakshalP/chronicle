1161c90e9b22e297697477b10d8b711ca0bc79f8
Dakshal P <dakshal29@gmail.com>
2023-05-27 08:54:32 -0400

    edit coming soon page

--- app/dashboard/coming-soon/page.tsx
+++ app/dashboard/coming-soon/page.tsx
@@ -1,15 +1,27 @@
 import Button from "@/components/Button";
+import { IoMdNotificationsOutline } from "react-icons/io";
+import { RiFlag2Line } from "react-icons/ri";
+import { HiOutlineLocationMarker } from "react-icons/hi"
+
+const comingSoon = {
+  "Hour Goals": [<RiFlag2Line className="w-8 h-8" />, "/dashboard/coming-soon"],
+    "Notifications": [<IoMdNotificationsOutline className="w-8 h-8" />, "/dashboard/coming-soon"],
+    "Return Visits": [<HiOutlineLocationMarker className="w-8 h-8" />, "/dashboard/coming-soon"],
+}
 
 export default function ComingSoon() {
   return (
     <div className="flex flex-col p-10 text-center justify-center gap-10 items-center min-h-screen bg-cgray dark:bg-gray-700">
                 <h1 className="font-extrabold text-6xl font-display tracking-wider">Coming Soon</h1>
                 <p>Features planned for a future version of Chronicle:</p>
-                <ul>
-                  <li>Hour Goals</li>
-                  <li>Notifications</li>
-                  <li>Return Visit Record</li>
-                </ul>
+                <div className="lg:block mx-auto flex flex-col justify-evenly h-1/2 w-fit">
+                    {Object.entries(comingSoon).map(([label, properties]) => (
+                        <span className="cursor-pointer lg:mr-5 lg:mb-5 lg:py-0 py-5 font-extrabold tracking-wide lg:text-xl text-2xl flex flex-row lg:justify-start justify-center items-center gap-5 w-full">
+                        {properties[0]}
+                        {label}
+                        </span>
+                    ))}
+                </div>
                 <Button variant={"cgreen"} href="/dashboard">Return to the service log</Button>
     </div>
   )
