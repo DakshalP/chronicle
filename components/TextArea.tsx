31b30f4c8c303de581156904f44f24937b61a6fd
Dakshal P <dakshal29@gmail.com>
2023-06-11 22:28:15 -0400

    add forwardRef merged with localRef in textarea

--- components/TextArea.tsx
+++ components/TextArea.tsx
@@ -1,30 +1,34 @@
 "use client"
 
-import { TextareaHTMLAttributes, FormEventHandler } from "react";
-import { useRef } from "react";
+import { TextareaHTMLAttributes, forwardRef, useState, useRef } from "react";
+import { mergeRefs } from "react-merge-refs"
 
 export interface TextareaProps extends TextareaHTMLAttributes<HTMLTextAreaElement> {
-    onInput?: undefined;
+    maxLength?: number;
 }
 
-const styles = `block p-2.5 w-full min-h-fit font-medium text-lg text-gray-900 bg-white rounded border-4 border-cgray resize-none outline-none transition-all
-focus:ring-cgreen focus:border-cgreen 
-dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-cgray-outline dark:focus:border-cgray-dark`
+const styles = `block p-2.5 w-full min-h-fit font-medium text-lg text-gray-900 bg-white resize-none outline-none dark:bg-gray-700 dark:placeholder-gray-400 dark:text-white `
 
-const TextArea: React.FunctionComponent<TextareaProps> = ({
+const TextArea = forwardRef<HTMLTextAreaElement, TextareaProps>(({
+    maxLength,
     className,
     ...props
-}) => {
-    const textAreaRef = useRef<HTMLTextAreaElement>(null);
+}, ref) => {
+    const localRef = useRef<HTMLTextAreaElement>(null);
+    const [count, setCount] = useState(0)
     const fitContent = () => {
-        if(textAreaRef.current) {
-            textAreaRef.current.style.height = "";
-            textAreaRef.current.style.height = (textAreaRef.current.scrollHeight + 9) + "px";
+        if(localRef.current) {
+            localRef.current.style.height = "";
+            localRef.current.style.height = (localRef.current.scrollHeight + 9) + "px";
+            setCount(localRef.current.value.length)
         }
     }
     return (            
-            <textarea ref={textAreaRef} onInput={fitContent} className={`${styles} ${className}`} {...props} />
+            <div className="transition-all rounded border-4 border-cgray dark:border-gray-600 focus-within:ring-cgreen focus-within:border-cgreen dark:focus-within:ring-cgray-outline dark:focus-within:border-cgray-dark">
+                <textarea ref={mergeRefs([localRef, ref])} maxLength={maxLength} onInput={fitContent} className={`${styles} ${className}`} {...props} />
+                {maxLength && <div className="text-end dark:bg-gray-700 bottom-0 bg-white p-2 text-gray-400 font-semibold text-sm">{count}/{maxLength}</div>}
+            </div>
     );
-};
+})
 
 export default TextArea;
