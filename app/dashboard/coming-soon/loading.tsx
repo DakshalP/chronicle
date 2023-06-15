24b8c8eced9629335a0f8670ebf14b6dcb2ec6c1
Dakshal P <dakshal29@gmail.com>
2023-06-15 13:29:45 -0400

    change name of loading page component

--- app/dashboard/coming-soon/loading.tsx
+++ app/dashboard/coming-soon/loading.tsx
@@ -1,5 +1,5 @@
 
-export default async function Dashboard() {
+export default async function Loading() {
     return (
             <div className="flex justify-center items-center min-h-screen min-w-screen bg-cgray dark:bg-gray-700">
                 <h1 className="font-extrabold text-6xl font-display tracking-wider animate-pulse">Loading...</h1>
